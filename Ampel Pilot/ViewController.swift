25514ae81d395d639d28cb5e6aaab89a2a445068
pvalenta <patrick.valenta@hs-augsburg.de>
2017-10-03 09:50:55 +0200

    Initial Commit

---
+++ Ampel Pilot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ampel Pilot
+//
+//  Created by Patrick Valenta on 03.10.17.
+//  Copyright © 2017 Patrick Valenta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Ampel PilotTests/Ampel_PilotTests.swift
@@ -0,0 +1,36 @@
+//
+//  Ampel_PilotTests.swift
+//  Ampel PilotTests
+//
+//  Created by Patrick Valenta on 03.10.17.
+//  Copyright © 2017 Patrick Valenta. All rights reserved.
+//
+
+import XCTest
+@testable import Ampel_Pilot
+
+class Ampel_PilotTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
